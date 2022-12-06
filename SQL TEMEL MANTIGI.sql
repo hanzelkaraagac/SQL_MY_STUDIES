ad14efbc5ace8311a56fa3719e47e77a7d1b2fb0
hanzelkaraagac <hanzelgamess@gmail.com>
2022-12-06 21:03:42 +0300

    2022-12-06

---
+++ SQL TEMEL MANTIGI.sql
@@ -0,0 +1,12 @@
+1.Data Query Language (Veri Sorgulama Dili - DQL)
+SELECT: veritabanındaki verileri alır.
+
+2.Data Manipulation Language (Veri Kullanma Dili - DML)
+INSERT:Veritabanına yeni veri ekler.
+DELETE:Veritabanındaki verileri siler
+UPDATE:Veritabanındaki verileri günceller.
+
+3.Data Definition Language (Veri Tanimlama Dili - DDL)
+DROP: Bir veritabanını veya veritabanı içindeki tabloyu siler.
+ALTER: Bir veritabanı veya veritabanı içindeki tabloyu günceller.
+CREATE:Bir veritabanı veya veritabanı içinde tablo oluşturur
